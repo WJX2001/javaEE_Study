54a82b6dd5200bc5528825ddde43e7d4b5227341
WJX2001 <1758807220@qq.com>
2024-06-30 23:39:16 +0800

    feat: java IO流学习

---
+++ myIo/src/com/wjx/myconvertstream/ConvernStreamDemo1.java
@@ -0,0 +1,15 @@
+package com.wjx.myconvertstream;
+
+import java.io.InputStreamReader;
+
+public class ConvernStreamDemo1 {
+
+  /*
+  * 利用转换流按照指定字符编码读取
+  * */
+
+  // 1. 创建对象并指定字符编码
+  public static void main(String[] args) {
+    InputStreamReader inputStreamReader = new InputStreamReader();
+  }
+}
